84b6bb2dab1415db5043656a017cd1cff55768ce
mllannes <mariilannes93@gmail.com>
2023-02-03 14:52:18 -0300

    Funcionamento de array

---
+++ adicionando-nota.js
@@ -0,0 +1,8 @@
+const notas = [10, 6, 8]
+
+//adicionando um valor na const notas:
+notas.push(7)
+
+const media = [notas[0] + notas[1] + notas[2] + notas[3]] / notas.length
+
+console.log(media)
---
+++ index.js
@@ -0,0 +1,27 @@
+//const arrayVazia = [];
+
+// console.log(arrayVazia)
+// console.log(arrayVazia.length)
+
+//JavaScript criou o array e a guardou na variável arrayVazia, mas seu length é 0.
+
+// const arrayVazia = [];
+// console.log(arrayVazia[0])
+
+//undefined
+
+// const arrayVazia = [, , ,]
+// console.log(arrayVazia.length)
+// console.log(arrayVazia[0])
+// console.log(arrayVazia[1])
+// console.log(arrayVazia[2])
+
+//Um array com 3 posições, porém em nenhuma dessas posições existe um valor.
+
+const arrayVazia = [, , ,]
+console.log(arrayVazia.length)
+
+arrayVazia.push(50)
+
+console.log(arrayVazia)
+console.log(arrayVazia.length)
