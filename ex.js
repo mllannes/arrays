b1efd5f0d715bf8f547dcd81caabbf40ce5b3c75
mllannes <mariilannes93@gmail.com>
2023-03-10 17:57:40 -0300

    Funcionamento de array

---
+++ ex.js
@@ -0,0 +1,29 @@
+/* const arrayOriginal = ['Maria', 'Carlos', 'Eduardo', 'Samanta']
+const arrayConcat = arrayOriginal.concat('André', 'Fernanda')
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+
+*/
+
+/* const arrayOriginal = ['Maria', 'Carlos', 'Eduardo', 'Samanta']
+const arrayConcat = arrayOriginal.concat(
+  ['André', 'Fernanda'],
+  ['Ricardo', 'Ana'],
+  ['Marcelo', 'Bia']
+)
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+*/
+
+/* const arrayOriginal = [50, 60, 70]
+const arrayConcat = arrayOriginal.concat([80, [90, 100]])
+
+console.log(arrayConcat)
+console.log(arrayOriginal)
+*/
+
+const funcionarios = ["Ana", "Juliana", "Leonardo"], [30,35,28]; 
+
+console.log (`${funcionarios[0][2]} tem ${funcionarios[1][2]} anos.`)
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,8 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio']
+const medias = [10, 8, 7.5, 9]
+
+const listaDeAlunosEMedias = [alunos, medias]
+
+console.log(
+  `O aluno(a) da posição 1 é: ${listaDeAlunosEMedias[0][1]}. A nota deste aluno(a) foi : ${listaDeAlunosEMedias[1][1]}.`
+)
